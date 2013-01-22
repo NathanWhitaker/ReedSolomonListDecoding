378bb1fd13f8164444cf2430b1e21614fbc92c06
Nathan Whitaker <whitaker.nathan@gmail.com>
2013-01-22 18:31:07 +0000

    Removed unrequired function through vectorisation and moving up to call

--- ListDecodingAlgorithms/Eval_msg_poly_factor.m
+++ ListDecodingAlgorithms/Eval_msg_poly_factor.m
@@ -4,9 +4,13 @@ function [ count,gen_msg ] = Eval_msg_poly_factor( msg,poly,m,t )
 length = 2^m-1;
 k = length - 2*t;
 count = 0;
+poly_size = size(poly,1);
 gen_msg = zeros(1,length);
 for i=1:length,
-    msg_gf = Eval_gf_poly_factor(poly,msg(i),m);
+    %%%%Evaluate polynomial at point
+    point_powers = gf(msg(i),m) .^ [1:poly_size];
+    msg_gf  = point_powers * poly;
+    %%%
     gen_msg(i) = double(msg_gf.x);
     result = msg(i) - gen_msg(i);
     if (result == 0),
