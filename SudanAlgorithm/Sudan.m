4404f9671721f9d20bd5e23d51b3b1ab22b66e36
Nathan Whitaker <whitaker.nathan@gmail.com>
2013-01-22 19:10:38 +0000

    Removal of debug messages

--- SudanAlgorithm/Sudan.m
+++ SudanAlgorithm/Sudan.m
@@ -18,32 +18,21 @@ List = gf(zeros(m+l*d+1,l+1,1)); %% List of Coefficient matricies, k possible
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% Q(X,Y) Relationship between X and Y %%%%%%%%%
 tStart = tic;
 for comb=1:iteration_limit, %% combination number
-    %tic;
     c_kj = assign_c_kj(comb,l,m,d);
-    %fprintf('Assign : %d\r',toc);
     i = 1;
     Q = gf(0,m);
-    %tic;
     while((i <= n) && (Q == 0)),
-        %tic
         Q = Q_Weighted_Sum(X(i),Y,c_kj,m,l,d);
-        %fprintf('Sum : %d\r',toc);
         Y = Y * alpha;
         i = i + 1;
     end;
-    %fprintf('While : %d\r',toc);
-    %tic;
     if ((Q == 0) && (i == n+1)), %% Q values for coefficients used are zero for all inpus
         List_size = List_size + 1;
-        if List_size == 1,
-            f = comb;
-        end;
         fprintf('Value for iteration %d\r',comb)
-        fprintf('Value for normalised %d\r',comb/f)
         List(:,:,List_size) = c_kj; %% Add coefficent matrix to list recorded
     end;
-    %fprintf('Cleanup : %d\r',toc);
 end;
+fprintf('Average Time : %d\r',toc(tStart)/iteration_limit);
 tried_factors = zeros(size(List,1),size(List,2),1);
 for i=1:size(List,3),
     factors = Factor_Multi_gf_poly(List(:,:,i));
