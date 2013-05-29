d82ccb35aa10c3f1f879c0cbbc8677fad885c8bc
Nathan Whitaker <whitaker.nathan@gmail.com>
2013-05-29 12:12:20 +0100

    Split image encoding and reconstruction into seperarte sections, and the ability to save encoded data to multiple files to be completed on different machines in parallel

--- TestingFramework/Image_Encoder.m
+++ TestingFramework/Image_Encoder.m
@@ -1,4 +1,4 @@
-function [Corrected_Data] = Image_Encoder(m,encoded_k,t,filename,error_count)
+function [Corrected_Data] = Image_Encoder(m,encoded_k,t,filename,error_count,decode_count)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  System Parameters  %%%%%%%%%%%%%%%%%%%%
 p = 2;   % Base Prime
 n = (p^m)-1; % Codeblock Size
@@ -23,9 +23,9 @@ for i=0:n,
 end;
 clear factor poly_value 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  Data Encoding     %%%%%%%%%%%%%%%%%%%%%
-data = imread(filename);
-dimensions = size(data);
-data_stream = reshape(data,1,[]);
+Original_Image = imread(filename);
+dimensions = size(Original_Image);
+data_stream = reshape(Original_Image,1,[]);
 data_stream_lower = min(n,mod(data_stream,(n+1)));
 data_stream_upper = max(0,floor(double(data_stream)/(n+1)));
 factors = [data_stream_lower;data_stream_upper];
@@ -50,29 +50,36 @@ for i=1:Test_Num,
     Corrupted_Image_Data(:,i) = factor_reference(:,Corrupt_Index(1,i)+1,Corrupt_Index(2,i)+1);
 end; 
 clear i j Corrupt_Index factor_reference i j error_location error_value
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  Data Correction  %%%%%%%%%%%%%%%%%%%%%
-Corrected_Image_Data = Run_Image_Test(m,t,encoded_k,Corrupted_Data);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  Data Reconstruction       %%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  Corrupted Data Reconstruction %%%%%%%%
 Corrupted_Image_Data = Corrupted_Image_Data.x;
 Corrupted_Image = Corrupted_Image_Data(1,:) + (n+1)*Corrupted_Image_Data(2,:);
 Corrupted_Image = uint8(reshape(Corrupted_Image,dimensions));
-
-Corrected_Image_Data = Corrected_Image_Data.x;
-Corrected_Image = Corrected_Image_Data(1,:) + (n+1)*Corrected_Image_Data(2,:);
-Corrected_Image = uint8(reshape(Corrected_Image,dimensions));
-clear Corrupted_Image_Data Corrected_Image_Data
+clear Corrupted_Image_Data
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  Data Export       %%%%%%%%%%%%%%%%%%%%%
 file_split=regexp(filename,'\/','split');
 filename = file_split{size(file_split,2)};
 file_split=regexp(filename,'\x2E','split'); %%Split by "."
 extension = file_split(2);
 file_split=regexprep(file_split(1),'\x20','_');
-directory = strcat(sprintf('./Data/Data_m_%d_k_%d_t_%d_',m,k,t),file_split);
+directory = strcat(sprintf('./Data/Data_m_%d_k_%d_t_%d_',m,encoded_k,t),file_split);
 directory = directory{1}; %cell to string
 if ~exist(directory, 'dir')
   mkdir(directory);
 end
-imwrite(data,strcat(sprintf('%s/',directory),file_split{1},'_original','.',extension{1}));
+imwrite(Original_Image,strcat(sprintf('%s/',directory),file_split{1},'_original','.',extension{1}));
 imwrite(Corrupted_Image,strcat(sprintf('%s/',directory),file_split{1},'_corrupted','.',extension{1}));
-imwrite(Corrected_Image,strcat(sprintf('%s/',directory),file_split{1},'_reconstucted','.',extension{1}));
+dlmwrite(sprintf('%s/Corrupted_Data.txt',directory), Corrupted_Data.x);
+dlmwrite(sprintf('%s/Dimensions.txt',directory), dimensions);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  Split Decode Over Multiple Computers %%
+if (decode_count<=1),
+    dlmwrite(sprintf('%s/Encoded_Data.txt',directory),enc_data.x);
+else
+    Sub_Test_Count = ceil(Test_Num/decode_count);
+    for i=1:decode_count,
+        encode_sub = enc_data(:,(i-1)*Sub_Test_Count+1:min(i*Sub_Test_Count,Test_Num));
+        dlmwrite(sprintf('%s/Encoded_Data_%d.txt',directory,i),encode_sub.x);
+    end;
+end;
+        
 end
--- TestingFramework/Image_Reconstruction.m
+++ TestingFramework/Image_Reconstruction.m
@@ -1,19 +1,23 @@
-function [image_data] = Image_Reconstruction(m,t,k,filename,data)
+function [Corrected_Image] = Image_Reconstruction(m,t,encoded_k,filename,Corrected_Image_Data)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  System Parameters  %%%%%%%%%%%%%%%%%%%%
 p = 2;   % Base Prime
+n = p^m-1;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  System Sections  %%%%%%%%%%%%%%%%%%%%%
+file_split=regexp(filename,'\/','split');
+filename = file_split{size(file_split,2)};
 file_split=regexp(filename,'\x2E','split'); %%Split by "."
 extension = file_split(2);
 file_split=regexprep(file_split(1),'\x20','_');
-directory = strcat(sprintf('./Data/Data_m_%d_k_%d_t_%d_',m,k,t),file_split);
-directory = directory{1};
+directory = strcat(sprintf('./Data/Data_m_%d_k_%d_t_%d_',m,encoded_k,t),file_split);
+directory = directory{1}; %cell to string
+if ~exist(directory, 'dir')
+  mkdir(directory);
+end
 dimensions = dlmread(sprintf('%s/Dimensions.txt',directory));
-%map = dlmread(sprintf('%s/Map.txt',directory));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  Data Manipulation  %%%%%%%%%%%%%%%%%%%%%
-data_stream = data(1,:) + (p^m)*data(2,:);
-data_reshape = reshape(data_stream,dimensions);
+Corrected_Image_Data = Corrected_Image_Data.x;
+Corrected_Image = Corrected_Image_Data(1,:) + (n+1)*Corrected_Image_Data(2,:);
+Corrected_Image = uint8(reshape(Corrected_Image,dimensions));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  Data Encoding     %%%%%%%%%%%%%%%%%%%%%
-extension{1}
-imwrite(double(data_reshape),strcat(sprintf('%s/',directory),file_split{1},'_reconstructed','.',extension{1}),extension{1});
-image_data = data_reshape;
+imwrite(Corrected_Image,strcat(sprintf('%s/',directory),file_split{1},'_reconstucted','.',extension{1}));
 end
