1f50612edd93fd079aba44da876e1552b002e73a
Nathan Whitaker <whitaker.nathan@gmail.com>
2013-06-06 14:33:00 +0100

    Correction from corrupted data to corrected data

--- CL_image_encode_and_decode.m
+++ CL_image_encode_and_decode.m
@@ -1,4 +1,5 @@
-%matlabpool('open','AttachedFiles','Run_Image_Test.m');
-Corrupted_Data = Image_Encoder(4,2,6,'images/CL.bmp',7,1);
+matlabpool('open','AttachedFiles','Run_Image_Test.m');
+Corrupted_Data = Image_Encoder(4,2,6,'images/CL.bmp',6,1);
 Corrected_Data = Run_Image_Test(4,6,2,'CL.bmp',Corrupted_Data);
-Image_Reconstruction(4,6,2,'CL.bmp',Corrupted_Data);
+Image_Reconstruction(4,6,2,'CL.bmp',Corrected_Data);
+matlabpool close
---
+++ IL_image_encode_and_decode.m
@@ -0,0 +1,5 @@
+matlabpool('open','AttachedFiles','Run_Image_Test.m');
+Corrupted_Data = Image_Encoder(4,2,6,'images/IL.bmp',6,1);
+Corrected_Data = Run_Image_Test(4,6,2,'IL.bmp',Corrupted_Data);
+Image_Reconstruction(4,6,2,'IL.bmp',Corrected_Data);
+matlabpool close
