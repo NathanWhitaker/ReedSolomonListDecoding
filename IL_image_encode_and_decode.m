e692c179d1626805200796faeafbba4b723b0f6a
Nathan Whitaker <whitaker.nathan@gmail.com>
2013-06-07 00:28:24 +0100

    Increased error limit

--- IL_image_encode_and_decode.m
+++ IL_image_encode_and_decode.m
@@ -1,5 +1,5 @@
 matlabpool('open','AttachedFiles','Run_Image_Test.m');
-for i=1:10,
+for i=1:18,
     Corrupted_Data = Image_Encoder(4,2,6,'images/IL.bmp',i,1);
     Corrected_Data = Run_Image_Test(4,6,2,Corrupted_Data);
     Image_Reconstruction(4,6,2,'IL.bmp',Corrected_Data,i);
