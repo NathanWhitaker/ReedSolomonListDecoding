9ccfb7edae0720fd717d423c22c5580fcd97a83b
Nathan Whitaker <whitaker.nathan@gmail.com>
2013-06-06 23:01:13 +0100

    Vectorisation of brute force, with multiple factor multiplication on factor mult

--- ListDecodingAlgorithms/Factor_mult.m
+++ ListDecodingAlgorithms/Factor_mult.m
@@ -5,15 +5,20 @@ function [ res_factor ] = Factor_mult(factor_1,factor_2,m)
 % factorisation function that is contained within MuPad. This is used by
 % passing the polynomial as a string to this function which is used inline
 % and the result is then parsed to retrieve the factors found
-
 res_factor = gf(zeros(size(factor_1,1),size(factor_1,2)),m);
-
-for i=1:size(factor_2),
-	if(factor_2(i) ~= 0),
-		extra_factor = circshift(factor_1,i-1);
-		extra_factor(1:i-1) = 0;
-		res_factor = res_factor.x + factor_2(i)*gf(extra_factor,m);
-	end;
+for j=1:size(factor_2,2),
+    current_factor = factor_2(:,j);
+    if (j==1),
+        base_factor = factor_1;
+    else
+        base_factor = res_factor;
+    end;
+    for i=1:size(factor_2,1),
+        if(factor_2(i) ~= 0),
+            extra_factor = circshift(base_factor,i-1);
+            extra_factor(1:i-1) = 0;
+            res_factor = res_factor.x + current_factor(i)*gf(extra_factor,m);
+        end;
+    end;
 end;
-
 end
--- ListDecodingAlgorithms/Factorisation/Factorise_gf_brute.m
+++ ListDecodingAlgorithms/Factorisation/Factorise_gf_brute.m
@@ -4,6 +4,10 @@ factor = [];
 zers = gf(zeros(size(poly,1),1),m);
 one = zers;
 one(1) = 1;
+eval_matrix = [];
+for i=0:n,
+    eval_matrix = [eval_matrix gf(i,m).^(0:size(poly,1)-1)'];
+end;
 %% For each polynomial that is passed in
 for j=1:size(poly,2),
     poly_div = poly(:,j);
@@ -15,38 +19,26 @@ for j=1:size(poly,2),
         irreducible = 1;
     end;
     while(~irreducible), 
-        factors_found = [];
         factor_product = one;
-		%% Evaluate polynomial at every point in finite field
-        for i=0:n,
-            x_powers = gf(i,m).^(0:size(poly_div,1)-1);
-            eval = x_powers * poly_div;
-			%% If zero then factor found and appended to factor list
-            if (eval == 0),
-                new_factor = zers;
-                new_factor(2) = 1;
-                new_factor(1) = i;
-                factors_found = [factors_found new_factor];
-                factor_product = Factor_mult(factor_product,new_factor,m);
-                factor_product = factor_product(:,size(factor_product,2));
-            end;
-            if (i==n),
-                if isempty(factors_found),
-                    irreducible = 1;
-                    factor = [factor poly_div];
-                    break;
-                end;
-                factor = [factor factors_found];
-				%% Divide polynomial by factors that have been identified in the most recent pass
-                [poly_div, r] = Euclid(poly_div,factor_product,m);
-                if(gf_poly_deg(poly_div,m) == 0),
-                    factor = [factor poly_div];
-                    irreducible = 1;
-                    break;
-                end;
-            end;
+        eval = find((eval_matrix' * poly_div)'==0);
+        if isempty(eval),
+            irreducible = 1;
+            factor = [factor poly_div];
+            break;
+        end;
+		%% If zero then factor found and appended to factor list
+        new_factors = gf(zeros(size(poly,1),size(eval,2)),m);
+        new_factors(2,:) = 1;
+        new_factors(1,:) = eval-1;
+        factor_product = Factor_mult(factor_product,new_factors,m);
+        factor = [factor new_factors];
+		%% Divide polynomial by factors that have been identified in the most recent pass
+        [poly_div, r] = Euclid(poly_div,factor_product,m);
+        if (gf_poly_deg(poly_div,m) <= 1),
+            irreducible = 1;
+            factor = [factor poly_div];
+            break;
         end;
     end;
 end;
 end
-
