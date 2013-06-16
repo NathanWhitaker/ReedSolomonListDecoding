b51a9c93b8829af83ca69a5f40e2c8d62fa8f064
Nathan Whitaker <whitaker.nathan@gmail.com>
2013-06-16 22:15:29 +0100

    CL testing of my algorithm

--- WhitakerAlgorithm/CL_im_nw.m
+++ WhitakerAlgorithm/CL_im_nw.m
@@ -1,10 +1,8 @@
-function [ ] = CL_im_nw()
+function [ ] = CL_im_nw(i)
 matlabpool open
 [inv_factors,x_mat,comb] = InvGen(4,6);
-for i=12:15,
-	Corrupted_Data = Image_Encoder(4,2,6,'images/CL.bmp',i,1);
-	Corrected_Whitaker_Data = Run_WhitakerAlgorithm(4,6,Corrupted_Data,inv_factors,comb,x_mat);
-	Image_Reconstruction_nw(4,6,'images/CL.bmp',Corrected_Whitaker_Data,i)
-end;
+Corrupted_Data = Image_Encoder(4,2,6,'images/CL.bmp',i,1);
+Corrected_Whitaker_Data = Run_WhitakerAlgorithm(4,6,Corrupted_Data,inv_factors,comb,x_mat);
+Image_Reconstruction_nw(4,6,'images/CL.bmp',Corrected_Whitaker_Data,i)
 matlabpool close
 
